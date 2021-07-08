36687c4ffcddc6cd0576ecef7c45ad38ee245eab
Shashidhar7407 <shashank.papishetty@gmail.com>
2021-07-08 17:54:24 +0530

    react day 1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import User from "./components/user";
+
+function App() {
+  return (
+    <div>
+      <h1>shashidhar</h1>
+      <User name="shashidhar" description="FSWT" />
+    </div>
+  );
+}
+
+export default App;
