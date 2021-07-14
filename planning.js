cdea8c7394c27b2031e904faa4762cbd2238f756
Shashidhar7407 <shashank.papishetty@gmail.com>
2021-07-14 18:05:31 +0530

    hero container

---
+++ planning.js
@@ -0,0 +1,19 @@
+//book my show planning
+// 1. we need resusable corousal
+// hero corousal
+// common carousal
+// cast and crew carousel
+
+//pages
+// home page
+// movie page
+// category pages
+// authentication loginna and register login
+// razor pay payment gateway
+
+//planning................................................................
+//layout
+//route
+//pages
+//components
+//HOC higher order components
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import User from "./components/user";
+import { Route } from "react-router-dom";
+
+// HOC
+import DefaultHOC from "./HOC/Default.HOC";
+
+// Components
+import Temp from "./components/temp";
 
 function App() {
   return (
-    <div>
-      <h1>shashidhar</h1>
-      <User name="shashidhar" description="FSWT" />
-      <button className="border-2 px-2 py-1 border-blue-500 rounded bg-blue-500 text-white hover:bg-transparent hover:border-2 hover:text-blue-500 outline-none focus:outline-none focus:ring focus:border-blue-300">
-        Hello Tailwind
-      </button>
-    </div>
+    <>
+      <DefaultHOC path="/" exact component={Temp} />
+    </>
   );
 }
 
